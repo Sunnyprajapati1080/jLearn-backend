15e4acb7faef449d10317f194712308ac6490e90
sunny <saurabh1080i@gmail.com>
2022-03-23 09:39:27 +0530

    created login endpoint and getuser endpoint

--- routes/auth.js
+++ routes/auth.js
@@ -21,21 +21,54 @@ router.post("/createuser", [
         if (await User.findOne({ email: req.body.email })) {
             return res.status(400).json({ error: "a user with this email already exists." })
         }
-        // if no errors. create user
+        // generate password hash
         const salt = await bcrypt.genSalt(10);
         const securePassword = await bcrypt.hash(req.body.password, salt)
+
+        // if no errors. create user
         const user = await User.create({
             name: req.body.name,
             email: req.body.email,
             password: securePassword
         })
-        const token = jwt.sign({id: user.id}, process.env.JWT_SECRET)
-        res.json({token})
+        // send jwt token
+        const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET)
+        res.json({ token })
 
     } catch (error) {
-        res.status(500).send({error:error.message})
+        res.status(500).send({ error: error.message })
     }
 
 })
 
+// login user using POST method. no login required.
+router.post("/login", [
+    body("email", "enter a valid email").isEmail(),
+    body("password", "password cannot be blank.").exists()
+], async (req, res) => {
+    // if errors send bad request and errors
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+        return res.send(errors)
+    }
+    try {
+        // find user with similar email
+        const user = await User.findOne({ email: req.body.email })
+        if (!user) {
+            return res.status(400).json({ error: "please try to login with correct credentials" })
+        }
+        // compare given password with user password
+        const comparePassword = await bcrypt.compare(req.body.password, user.password)
+        if (!comparePassword) {
+            return res.status(400).json({ error: "please try to login with correct credentials" })
+        }
+        // if succeess. finally send jwt token.
+        const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET)
+        res.json({ token })
+
+    } catch (error) {
+        res.status(500).send({ error: error.message })
+    }
+
+})
 module.exports = router
